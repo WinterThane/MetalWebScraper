7e202f8a4f71ba4985e3573788b623a2e1253d17
WinterThane <demonsaso@gmail.com>
2018-05-15 21:53:03 +0200

    Write json file

--- scrapeAlphabet.py
+++ scrapeAlphabet.py
@@ -1,13 +1,19 @@
 from bs4 import BeautifulSoup
 import urllib
+import json
 
 url = urllib.request.urlopen('https://www.metal-archives.com/browse/letter')
-content = url.read()
-soup = BeautifulSoup(content, "html.parser")
+soup = BeautifulSoup(url.read(), "html.parser")
 
 tmp = soup.find_all('a')
 lettersList = tmp[23:]
 
-with open('BandLetters.txt', 'w') as outfile:   
+result = []
+
+def CleanList():
     for link in lettersList:
-        outfile.write('%s\n' % link.get('href'))
+        result.append(link.get('href'))
+
+with open('BandLetters.txt', 'w') as outfile:   
+    CleanList()
+    json.dump(result, outfile, indent=1)
