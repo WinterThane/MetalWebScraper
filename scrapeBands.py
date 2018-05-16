d6783ecad9f07fd6c7ec9d6d4fcf7f212e63ed57
WinterThane <demonsaso@gmail.com>
2018-05-16 15:41:00 +0200

    Reworked

--- scrapeBands.py
+++ scrapeBands.py
@@ -2,28 +2,27 @@ from bs4 import BeautifulSoup
 import urllib
 import json
 
-url = urllib.request.urlopen('https://www.metal-archives.com/browse/ajax-letter/l/Z/json/1?sEcho=1&iColumns=4&sColumns=&iDisplayStart=0')
-tmp = ""
-recNumber = 0
-x = []
+urlPrefix = "https://www.metal-archives.com/browse/ajax-letter/l/"
+urlLetter = "Z"
+urlSuffix = "/json/1?sEcho=1&iColumns=4&sColumns=&iDisplayStart=0"
 
-def GetRecNumberLine():
+url = urllib.request.urlopen(urlPrefix + urlLetter + urlSuffix)
+
+def GetRecNumberLine(url):
     counter = 0
-    global tmp
     for line in url:
         if counter == 1:
-            tmp = line
-            break
+            return line
         counter += 1
 
-def GetRecNumber():
-    global recNumber
-    for d in tmp.split():
+tmp = GetRecNumberLine(url)
+
+def GetRecNumber(line):
+    for d in line.split():
         d = d.decode("utf-8").replace(",", "")
         if d.isdigit():
-            recNumber = d
+            return d
 
-GetRecNumberLine()
-GetRecNumber()
+recNumber = GetRecNumber(tmp)
 
 print(recNumber)
