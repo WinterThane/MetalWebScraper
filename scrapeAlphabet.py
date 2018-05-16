74d855885c833f995c38670f2f895d6de9207e9d
WinterThane <demonsaso@gmail.com>
2018-05-16 16:23:59 +0200

    No need for json at this point

--- scrapeAlphabet.py
+++ scrapeAlphabet.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import urllib
-import json
 
 url = urllib.request.urlopen('https://www.metal-archives.com/browse/letter')
 soup = BeautifulSoup(url.read(), "html.parser")
@@ -8,12 +7,6 @@ soup = BeautifulSoup(url.read(), "html.parser")
 tmp = soup.find_all('a')
 lettersList = tmp[23:]
 
-result = []
-
-def CleanList():
-    for link in lettersList:
-        result.append(link.get('href'))
-
 with open('BandLetters.txt', 'w') as outfile:   
-    CleanList()
-    json.dump(result, outfile, indent=1)
+    for link in lettersList:
+        outfile.write('%s\n' % link.get('href'))
