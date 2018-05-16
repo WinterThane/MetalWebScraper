0e239d263d98e27f45eed721f1a487a2ba573092
WinterThane <demonsaso@gmail.com>
2018-05-16 18:42:47 +0200

    Fixed band name and link

--- scrapeBands.py
+++ scrapeBands.py
@@ -1,6 +1,24 @@
-import urllib
-import urllib.request
-import json
+import urllib, urllib.request, json
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
 
 urlPrefix = "https://www.metal-archives.com/browse/ajax-letter/l/"
 urlLetter = "Z"
@@ -9,7 +27,7 @@ outputFile = urlLetter + "_Bands.txt"
 url = urllib.request.urlopen(urlPrefix + urlLetter + urlSuffix + "0")
 
 
-def GetRecNumberLine(lines):
+def get_rec_number_line(lines):
     counter = 0
     for line in lines:
         if counter == 1:
@@ -17,18 +35,19 @@ def GetRecNumberLine(lines):
         counter += 1
 
 
-def GetRecNumber(line):
+def get_rec_number(line):
     for d in line.split():
         d = d.decode("utf-8").replace(",", "")
         if d.isdigit():
             return d
 
 
-tmp = GetRecNumberLine(url)
+tmp = get_rec_number_line(url)
 result = []
 
-def GetRecords():
-    recNumber = int(GetRecNumber(tmp))
+
+def get_records():
+    recNumber = int(get_rec_number(tmp))
     counter = 0
     while counter < recNumber:
         with urllib.request.urlopen(urlPrefix + urlLetter + urlSuffix + str(counter)) as url:
@@ -37,12 +56,28 @@ def GetRecords():
             counter += 500
 
 
-GetRecords()
+def fix_band_name(xx):
+    x = BeautifulSoup(xx, "html.parser")
+    name = x.get_text()  
+    for link in x.find_all('a'):
+        lnk = link.get('href')       
+    return name, lnk
 
 
-def WriteToFile():
+def make_new_json():
+    get_records()
+    counter = 1
+    for lines in result:   
+        for line in lines:
+            nameLink = fix_band_name(line[0])
+            print("{0}: {1}, {2}, {3}, {4}, {5}".format(counter, nameLink[0], nameLink[1], line[1], line[2], strip_tags(line[3])))
+            counter += 1
+
+
+def write_to_file():
     with open(outputFile, 'w') as outfile:   
         json.dump(result, outfile, indent=1)
 
 
-WriteToFile()
+make_new_json()
+#write_to_file()
