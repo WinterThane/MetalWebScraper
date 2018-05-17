53089a75445dda2bcf4597c891fdb8622a095cb8
WinterThane <demonsaso@gmail.com>
2018-05-17 09:01:48 +0200

    Added function for letter extraction

--- scrapeAlphabet.py
+++ scrapeAlphabet.py
@@ -1,12 +1,35 @@
 from bs4 import BeautifulSoup
 import urllib
 
+
 url = urllib.request.urlopen('https://www.metal-archives.com/browse/letter')
 soup = BeautifulSoup(url.read(), "html.parser")
 
+
 tmp = soup.find_all('a')
 lettersList = tmp[23:]
+output = []
 
-with open('BandLetters.txt', 'w') as outfile:   
+
+def extract_link():   
     for link in lettersList:
-        outfile.write('%s\n' % link.get('href'))
+        output.append(link.get('href'))
+
+
+def extract_letters():
+    extract_link()
+    tmp = []
+    for line in output:
+        x = line.split("/")
+        tmp.append(x[-1])
+    return tmp
+
+
+def write_file():
+    with open('Alphabet.txt', 'w') as outfile:   
+        extract_link()
+        for link in output:
+            outfile.write('%s\n' % link)
+
+
+write_file()
